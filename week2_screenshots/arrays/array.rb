d2434dc96078f59973bc5bb747f66dc3d3924463
Matthew Thorne <mwilsonthorne@gmail.com>
2018-03-24 21:45:18 +0000

    Submitted Week 2 & Week 3 Screenshots

---
+++ week2_screenshots/arrays/array.rb
@@ -0,0 +1,6 @@
+solar_system = ["Mercury", "Venus", "Earth", "Mars", "Jupiter",
+                "Saturn", "Uranus", "Neptune", "Pluto"]
+
+solar_system.pop
+
+p solar_system
---
+++ week2_screenshots/hashes/hashes.rb
@@ -0,0 +1,10 @@
+dist_from_earth = {"Three Billion Miles" => "Neptune",
+                "Two Billion Miles"=> "Uranus",
+                "One Billion Miles" => "Saturn",
+                "Half a Billion Miles" => "Jupiter",
+                "50 Million Miles" => "Mars",
+                "25 Million Miles" => "Venus",
+                "40 Million Miles" => "Mercury"}
+
+
+p dist_from_earth.length
